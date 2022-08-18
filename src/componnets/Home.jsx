200c9e30f26221becb4e0f211b39701e68e47387
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-08-18 18:08:13 +0200

    anfang

---
+++ src/componnets/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+        <Link to='/login'> login</Link>
+       <Link to='/register'> register</Link>
+       
+
+
+    </div>
+  )
+}
+
+export default Home
